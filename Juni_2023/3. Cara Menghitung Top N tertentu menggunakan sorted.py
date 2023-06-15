fd292943c1138baf5e781d8dc4be20ec8c1ce1d0
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-06-16 00:44:52 +0700

    Update 3. Cara Menghitung Top N tertentu menggunakan sorted.py

--- Juni_2023/3. Cara Menghitung Top N tertentu menggunakan sorted.py
+++ Juni_2023/3. Cara Menghitung Top N tertentu menggunakan sorted.py
@@ -1,4 +1,4 @@
-Disini kita menghitung Top 5 Business dengan review terbanyak
+1. Disini kita menghitung Top 5 Business dengan review terbanyak
 
 # Import your libraries
 import pandas as pd
@@ -24,3 +24,20 @@ sort_values = mengurutkan data
 review_count = kolom yang dipakai acuan untuk mengurutkan data
 ascending = False <- hasil yang diinginkan adalah descending 
 sorted_list.head() = menampilkan 5 terbesar saja
+
+2. Disini kita menghitung Top 5 States dengan jumlah 5 Stars terbanyak
+
+# Start writing code
+yelp_business.head()
+# grouping by
+yb_group = yelp_business.groupby(['state','stars']).size().reset_index(name = "n_business") ## grouping + convert series to dataframe type
+yb_group
+
+yb_group[yb_group['stars'] == 5] ## conditional_formatting
+
+# sort by count
+ya_group = yb_group[yb_group['stars'] == 5].sort_values('n_business', ascending = False) ## conditional_formatting + sorting
+
+# select only desired column
+ya_selected = ya_group[['state','n_business']]
+ya_selected.head(6)
